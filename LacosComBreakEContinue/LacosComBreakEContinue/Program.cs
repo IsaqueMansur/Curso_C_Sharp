21d45e59d133b845b5afcd21053f55ed832340c7
IsaqueMansur <isaquemansur@gmail.com>
2023-05-02 16:41:50 -0300

    break and continue using loopings

---
+++ LacosComBreakEContinue/LacosComBreakEContinue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacosComBreakEContinue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Pressione 'c' para continuar e 's' para sair");
+                string pressionedKey = Console.ReadKey(true).KeyChar.ToString();
+                if (pressionedKey == "c")
+                {
+                    continue;
+                }
+                else if (pressionedKey == "s")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Tecla desconhecida");
+                }
+                Console.Clear();
+            }
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+        }
+    }
+}
