53cccc07137d6a92d441c24297f909e985b0c048
IsaqueMansur <isaquemansur@gmail.com>
2023-05-03 11:49:41 -0300

    Struct

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Program
+    {
+        enum DaysOfWeek_e
+        {
+            Domingo = 1,
+            Segunda,
+            Terça,
+            Quarta,
+            Quinta,
+            Sexta,
+            Sábado,
+        }
+
+        static void Main(string[] args)
+        {
+            DaysOfWeek_e day = DaysOfWeek_e.Segunda;
+            Console.WriteLine(day + " é o dia " + Convert.ToUInt32(day) + " da semana.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    internal class Program
+    {
+        struct DatasOfCadaster
+        {
+            public string name;
+            public string nameOfWay;
+            public UInt32 NumberOfHome;
+            public DateTime dateOfBirth;
+        }
+        static void Main(string[] args)
+        {
+            DatasOfCadaster myCadaster;
+            myCadaster.name = "Isaque Mansur";
+            myCadaster.nameOfWay = "Pç Guanabara";
+            myCadaster.NumberOfHome = 54;
+            myCadaster.dateOfBirth = Convert.ToDateTime("31/10/2000");
+            Console.WriteLine(myCadaster.name);
+            Console.ReadKey();
+        }
+    }
+}
