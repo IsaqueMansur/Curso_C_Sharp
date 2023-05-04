34941af0a0b669e82ccf8c532a03de80374cf097
IsaqueMansur <isaquemansur@gmail.com>
2023-05-04 09:45:42 -0300

    Exceptions

---
+++ Excecoes/Excecoes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Digire um número: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+            int result = 10 / key;
+            Console.WriteLine(result);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Final do tratamento da exceção.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
