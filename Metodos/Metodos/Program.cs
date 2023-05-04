00a156d2af4b08bc6fabd1dd76da6b79c4d1a94d
IsaqueMansur <isaquemansur@gmail.com>
2023-05-04 10:25:40 -0300

    Methods

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static void Multiply(double a, double b, ref double resultOfOperation) 
+        {
+            resultOfOperation = a * b;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(1, 2));
+
+            double initialResult = 0;
+            Multiply(10, 10, ref initialResult);
+            Console.WriteLine(initialResult);
+
+            Console.ReadKey();
+        }
+    }
+}
