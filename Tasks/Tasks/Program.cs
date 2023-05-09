d15e504ad696d92a23dcbe7c9ffa8db3beb34042
IsaqueMansur <isaquemansur@gmail.com>
2023-05-09 15:28:43 -0300

    Tasks

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Program
+    {
+        static int PrintMessage(int counter)
+        {
+            for (int i = counter; i < (counter + 10); i++)
+            {
+                Console.WriteLine("Task: " + i.ToString());
+                Thread.Sleep(500);
+            }
+            return 10;
+        }
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray; 
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            int resultOfTask = 0;
+            Task taskPrintMessage = Task.Run(() => resultOfTask = PrintMessage(100));
+            if(!taskPrintMessage.Wait(10000)) 
+            {
+                Console.WriteLine("A task não foi finalizada ainda");
+            }
+            else
+            {
+                Console.WriteLine(resultOfTask);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Threads(Avancado)/Threads(Avancado)/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads_Avancado_
+{
+    internal class ParametersThread
+    {
+        public int StartCounter
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public ParametersThread(int startCounter, string name)
+        {
+            StartCounter = startCounter;
+            Name = name;
+        }
+    }
+    internal class Program
+    {
+        public static Mutex mutex = new Mutex();
+        public static void Thread1()
+        {
+            int counter = 0;
+            do
+            {
+                Console.WriteLine(counter);
+                mutex.WaitOne();
+                counter++;
+                Thread.Sleep(100);
+                mutex.ReleaseMutex();
+
+            } while (counter <= 100);
+        }
+
+        public static void Thread2(object initialCounter)
+        {
+            int counter = (int)initialCounter;
+            do
+            {
+                Console.WriteLine(counter);
+                mutex.WaitOne();
+                counter++;
+                Thread.Sleep(100);
+                mutex.ReleaseMutex();
+
+            } while (counter <= ((int)initialCounter + 100));
+        }
+
+        public static void Thread3(int counter, string name)
+        {
+            do
+            {
+                Console.WriteLine("Nome: " + name + ". TimerInformado: " + counter);
+                mutex.WaitOne();
+                counter++;
+                Thread.Sleep(100);
+                mutex.ReleaseMutex();
+
+            } while (counter <= (counter + 100));
+        }
+        static void Main(string[] args)
+        {   
+            Thread thread1 = new Thread(new ThreadStart(Thread1));
+            Thread thread2 = new Thread(new ParameterizedThreadStart(Thread2));
+            Thread thread3 = new Thread(() => Thread3(100, "Isaque"));
+
+            thread1.Start();
+            thread2.Start(100);
+            thread3.Start();
+
+            Console.ReadKey();
+            Console.Clear();
+            
+        }
+    }
+}
