65ded4aea0b5eb77d1e0e88a153476a001029315
IsaqueMansur <isaquemansur@gmail.com>
2023-05-09 13:58:07 -0300

    Threads and Sleep

---
+++ ProgramacaoParelela/ProgramacaoParelela/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProgramacaoParelela
+{
+    internal class Program
+    {
+        static Thread t1;
+        static Thread t2;
+        static bool finishLooping;
+        static UInt16 numberOfThread;
+        static object objLock;
+        static Mutex mutex;
+
+        static void MyThread1()
+        {
+            while(!finishLooping)
+            {
+                mutex.WaitOne();
+                numberOfThread = 1;
+                Console.WriteLine("THREAD1: Passou 1 segundo, número da THREAD1:" + numberOfThread);
+                Thread.Sleep(1000);      
+                mutex.ReleaseMutex();
+            }
+        }
+
+        static void MyThread2()
+        {
+            while (!finishLooping)
+            {
+                mutex.WaitOne();
+                numberOfThread = 2;
+                Console.WriteLine("THREAD2: Passou 1 segundo, número da THREAD2:" + numberOfThread);
+                Thread.Sleep(1000);
+                mutex.ReleaseMutex();                
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i ++)
+            {
+                Thread.Sleep(10);
+                Console.WriteLine("Passou um segundo ("+(i + 1)+").");
+            }
+
+            finishLooping = false;
+            objLock = new object();
+            mutex = new Mutex();
+
+            t1 = new Thread(new ThreadStart(MyThread1));
+            t2 = new Thread(new ThreadStart(MyThread2));
+            t1.Priority = ThreadPriority.BelowNormal;
+            t2.Priority = ThreadPriority.Highest;
+
+            t1.Start();
+            t2.Start();
+
+            Console.ReadKey();
+            finishLooping = true;
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ProgramacaoParelela/ProgramacaoParelela/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ProgramacaoParelela")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProgramacaoParelela")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("4d2589a8-0394-47aa-aa0d-1a00c31934d2")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
