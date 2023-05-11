fb05a3e96cab3f668cacf50b589f54b5b9190ca4
IsaqueMansur <isaquemansur@gmail.com>
2023-05-11 09:36:53 -0300

    Async and Await

---
+++ AsyncAwait/AsyncAwait/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+
+    internal class Program
+    {
+        public static async Task<string> BuyCar(string nameClient)
+        {
+            Console.WriteLine(nameClient + " Foi comprar um carro");
+            await Task.Delay(1000);
+            Console.WriteLine(nameClient + " Está comprando um carro");
+            await Task.Delay(1000);
+            Console.WriteLine(nameClient + " Comprou um carro");
+            await Task.Delay(1000);
+            return "Carro comprado";
+
+        }
+        public static async void Party()
+        {
+            string car = await BuyCar("Isaque");
+            Console.WriteLine(car);
+        }
+        static void Main(string[] args)
+        {
+            Party();
+            Console.ReadKey();
+        }
+    }
+}
