9ae370ccee67be19c946fa78a840f8a02ec9eef6
IsaqueMansur <isaquemansur@gmail.com>
2023-05-11 09:49:58 -0300

    Timers using System.Timers and System.Threading

---
+++ Timers/Timers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Timers
+{
+    internal class Program
+    {
+        static void TimerTick(object sender, EventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+        }
+
+        static void TimerTick2(object state)
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss")); 
+        }
+        static void Main(string[] args)
+        {
+            //Timer com System.Timers
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.AutoReset = true;
+            timer.Elapsed += TimerTick;
+            timer.Start();
+            Console.ReadKey();
+            timer.Stop();
+
+            //Timer com System.Threading
+            TimerCallback tcb = new TimerCallback(TimerTick2);
+            System.Threading.Timer myTimer = new System.Threading.Timer(tcb, null, 0, 1000);
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+            myTimer.Dispose();
+        }
+    }
+}
